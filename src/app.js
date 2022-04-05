1d508c35e2a62fb91c818d4e6286f4a063a8833a
AnyelinaParedes <apg15194@hotmail.com>
2022-04-05 14:09:32 +0000

    generador de excuse

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let who2 = Math.floor(Math.random() * who.length);
+  let action2 = Math.floor(Math.random() * action.length);
+  let what2 = Math.floor(Math.random() * what.length);
+  let when2 = Math.floor(Math.random() * when.length);
+
+  document.getElementById("excuse").innerHTML =
+    who[who2] + action[action2] + what[what2] + when[when2];
   //write your code here
   console.log("Hello Rigo from the console!");
 };
